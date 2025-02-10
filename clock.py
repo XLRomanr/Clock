d4c9db65b81262b2b6e10de2c83bbace29463de5
Liangrui Xu <3222608168@qq.com>
2025-02-10 12:07:25 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,6 +1,33 @@
 import streamlit as st
 import time
 from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+from threading import Thread
+
+def update_access_time():
+    appid = "Plot_definition"  # 你应该替换成该 Streamlit 应用的 ID
+    flask_server_url = "http://11.2.171.248:5000"  # 改成你的 Flask 服务器 IP
+    url = f"{flask_server_url}/api/apps/{appid}/update_access_time"
+    
+    try:
+        response = requests.post(url)
+        if response.status_code == 200:
+            st.success("访问时间已更新")
+        else:
+            st.warning("无法更新访问时间")
+    except Exception as e:
+        print(f"Failed to update access time: {e}")
+
+# 后台线程定时调用更新访问时间
+def run_periodic_update():
+    while True:
+        update_access_time()
+        time.sleep(600)  # 每 10 分钟更新一次访问时间
+
+# 启动后台线程（守护线程）
+Thread(target=run_periodic_update, daemon=True).start()
 
 # 设置页面配置
 st.set_page_config(
